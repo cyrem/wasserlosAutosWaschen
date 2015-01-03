288f0a8eb7a80ad13d74ab77f0997df8a8a99166
matAdmin <matAdmin@des>
2015-01-03 17:01:22 +0100

    getFavorites load data from mysql

--- meilenstein6/php/getFavorites.php
+++ meilenstein6/php/getFavorites.php
@@ -1,37 +1,43 @@
 <?php
-
 header ( "Content-type: text/javascript" );
 
-$verarbeiteJson = function($data){
-    if($data != ""){
-        
-        $decoded = json_decode($data,true);
-        foreach($decoded[0] as $k=>$v){
-            $newArr[$k] = $k;
-        }
-        $retArr[] =$newArr;
-        foreach($decoded as $d){
-        $retArr[]= $d;
-        }
-        return json_encode($retArr);
-    }
-
+$verarbeiteJson = function ($data) {
+	if ($data != "") {
+		
+		$decoded = json_decode ( $data, true );
+		foreach ( $decoded [0] as $k => $v ) {
+			$newArr [$k] = $k;
+		}
+		$retArr [] = $newArr;
+		foreach ( $decoded as $d ) {
+			$retArr [] = $d;
+		}
+		return json_encode ( $retArr );
+	}
 };
 
 if (isset ( $_GET ['data'] )) {
-	//{"Filmtitel":"Filmtitel","Regie":"Regie","Drehbuch":"Drehbuch","Erscheinungsjahr":"Erscheinungsjahr","Genre":"Genre"},
+	
+	// connection
+	$host = "127.0.0.1";
+	$user = "localhost";
+	$pwd = "localhost";
+	$dbName = "waw";
+	$con = new MySQLi ( $host, $user, $pwd, $dbName );
+	$con->set_charset ( "UTF-16" );
+	
+	// {"Filmtitel":"Filmtitel","Regie":"Regie","Drehbuch":"Drehbuch","Erscheinungsjahr":"Erscheinungsjahr","Genre":"Genre"},
 	if ($_GET ['data'] == "music") {
-		//echo $verarbeiteJson(file_get_contents('music.txt'));
-		echo file_get_contents('../js/musik.json');
-	}
-	if ($_GET ['data']== "movies") {
-		//echo $verarbeiteJson(file_get_contents('film.txt'));
-		echo file_get_contents('../js/film.json');
 		
+		$result = mysqli_fetch_all ( $con->query ( "SELECT * FROM film ORDER BY filmId DESC LIMIT 0 ,20" ) );
+	}
+	if ($_GET ['data'] == "movies") {
+		$result = mysqli_fetch_all ( $con->query ( "SELECT * FROM album ORDER BY albumId DESC LIMIT 0 ,20" ) );
+	}
+	if ($result) {
+		echo json_encode ( $result );
 	}
 }
-
-
 
 ?>
 
